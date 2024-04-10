c22d60c10a218add0116d80481e41a6dd5ab39f5
Bibby Chung <bibbynet@gmail.com>
2024-04-10 12:35:51 +0800

    Revert "feat: add pwa"
    This reverts commit f9cd011faa92890d89de11587326ad678008f311.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+} from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
@@ -7,7 +9,6 @@ import {
 } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,9 +18,5 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       withPreloading(PreloadAllModules),
     ),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
   ],
 };
