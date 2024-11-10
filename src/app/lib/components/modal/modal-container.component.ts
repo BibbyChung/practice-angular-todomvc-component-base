db9bb171908dd65905d0945571dae4a4423a333b
Bibby Chung <bibbynet@gmail.com>
2024-11-10 16:43:53 +0800

    feat: add the fade modal feature
    feat: add the fade modal feature

--- src/app/lib/components/modal/modal-container.component.ts
+++ src/app/lib/components/modal/modal-container.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule, NgComponentOutlet } from '@angular/common'
 import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { getModals } from '../../services/layout.service'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { delay, tap } from 'rxjs'
+import { getBehaviorSubject, getSubject } from '../../common/utils'
+import { closeModal, getModals } from '../../services/layout.service'
 
 @Component({
   selector: 'bb-modal-container',
@@ -9,9 +12,22 @@ import { getModals } from '../../services/layout.service'
   template: `
     @for (item of modals$ | async; track $index) {
       <div class="fixed inset-0 z-20 h-screen w-screen overflow-y-auto">
-        <div class="fixed inset-0 bg-black opacity-40"></div>
-        <div class="fixed flex min-h-screen min-w-full items-center justify-center px-4 py-12">
-          <ng-container *ngComponentOutlet="item.component; inputs: { props: item.props }" />
+        <!-- eslint-disable-next-line @angular-eslint/template/elements-content -->
+        <button
+          class="fixed inset-0 bg-black opacity-0 transition-opacity duration-150 ease-in"
+          (click)="btnCloseModal$.next(true)"
+          [class]="{ 'opacity-60': isOpacity$ | async }"
+        ></button>
+        <div
+          class="pointer-events-none fixed flex min-h-screen min-w-full items-center justify-center px-4 py-12"
+        >
+          <div
+            class="pointer-events-auto opacity-0 transition-opacity duration-150 ease-in"
+            [class]="{ 'opacity-100': isOpacity$ | async }"
+          >
+            <img class="hidden" src alt="" (error)="triggerOpacity$.next(true)" />
+            <ng-container *ngComponentOutlet="item.component; inputs: { props: item.props }" />
+          </div>
         </div>
       </div>
     }
@@ -21,5 +37,19 @@ import { getModals } from '../../services/layout.service'
 })
 export class ModalContainerComponent {
   modals$ = getModals()
-  constructor() {}
+  triggerOpacity$ = getBehaviorSubject(false)
+  btnCloseModal$ = getSubject<boolean>()
+
+  isOpacity$ = this.triggerOpacity$.pipe(delay(10))
+
+  closeModalSub = this.btnCloseModal$
+    .pipe(
+      takeUntilDestroyed(),
+      tap(() => this.triggerOpacity$.next(false)),
+      delay(150),
+      tap(() => {
+        closeModal()
+      })
+    )
+    .subscribe()
 }
