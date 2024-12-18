479147898896a7b1c569396c075fd3573e72fbe6
Bibby Chung <bibbynet@gmail.com>
2024-12-18 17:58:49 +0800

    feat: add zonejs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core'
+import { ApplicationConfig } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
     provideRouter(
       routes,
       withComponentInputBinding()
