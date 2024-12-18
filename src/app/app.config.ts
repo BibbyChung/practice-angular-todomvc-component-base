9abe4e025cc00737448c78a31c55f8f87ba9ffad
Bibby Chung <bibbynet@gmail.com>
2024-12-19 00:55:09 +0800

    feat: remove zonejs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(
       routes,
       withComponentInputBinding()
